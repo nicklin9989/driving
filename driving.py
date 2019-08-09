4b6281e94880bb523b066c03d7ce56bc0deac330
Nick Lin <a778977@gmail.com>
2019-08-09 21:44:40 +0800

    us part

--- driving.py
+++ driving.py
@@ -6,3 +6,8 @@ if country == '台灣' :
         print('你可以考駕照')
     else:
         print('你不能考駕照Ｒ')
+elif country == '美國' :
+    if age >= 16:
+        print('你就可以開車')
+    else:
+        print('你還太嫩回家跟媽媽要錢吧')
